7bbd890fddb6ab3ff15229ccf3b42243ed2c77a9
ANastyaInvSyn <nastya.kurina@gmail.com>
2025-10-14 23:38:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Получаем токен из переменных окружения (для хостинга) или используем локальный
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8392912343:AAH9Cwbsc_6BujWFGXKULrWGgfXg17s4bOc')
 TIMEZONE = "Europe/Moscow"
 REMINDER_TIMES = [5, 15, 30, 60]
 
